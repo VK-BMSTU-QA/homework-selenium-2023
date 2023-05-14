d61872f32ecacc7759a9150e591285df8adc7e6d
Ivan Biryukov <vanyabiryukov@yandex.ru>
2023-05-14 22:43:34 +0300

    fix selectors film unauth

--- hw/code/test_film_unauth.py
+++ hw/code/test_film_unauth.py
@@ -9,62 +9,49 @@ from const import *
 from helper_auth import needed_auth
 
 class TestFilmPageUnauthorized(BasePage):
-    X_BUTTON_PLUS = '/html/body/div/div/div[1]/div/div[2]/div[7]/div/a/div'
-    X_BUTTON_BOOKMARK = '/html/body/div/div/div[1]/div/div[2]/div[7]/a[2]/div'
-    X_BUTTON_TRAILER = '/html/body/div/div/div[1]/div/div[2]/div[7]/a[1]/div'
-    X_PRODUCER = '/html/body/div/div/div[1]/div/div[2]/div[5]/div[2]'
-    X_PRODUCER_ON_PAGE = '/html/body/div/div/div[1]/div/div/div/div[1]'
-    X_TOSTER = '/html/body/div/div[2]'
-    X_TRAILER_CONTAINER = '/html/body/div/div[1]/div/div/iframe'
-    X_BUTTON_RATE_9 = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[2]/div[1]/button[2]'
-    X_BUTTON_RATE_1 = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[2]/div[1]/button[10]'
-    X_BUTTON_RATE_CONTAINER = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[2]/div[1]'
-    X_BUTTON_RATE_DELETE = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[4]/button'
-    X_BUTTON_REVIEW = '/html/body/div/div/div[4]/div[1]/div/div/div[1]/a/div'
-    X_BUTTON_REVIEW_CONTAINER = '/html/body/div/div[1]/div/div/div/div'
-    X_BUTTON_CHOOSE_TYPE = '/html/body/div/div[1]/div/div/div/div/form/div[1]/div/div'
-    X_BUTTON_NEGATIVE_TYPE = '/html/body/div/div[1]/div/div/div/div/form/div[1]/div/ul/li[3]'
-    X_BUTTON_NEUTRAL_TYPE = '/html/body/div/div[1]/div/div/div/div/form/div[1]/div/ul/li[2]'
-    X_BUTTON_POSITIVE_TYPE = '/html/body/div/div[1]/div/div/div/div/form/div[1]/div/ul/li[1]'
-    X_INPUT_TITLE = '/html/body/div/div[1]/div/div/div/div/form/div[2]/input'
-    X_INPUT_TEXT = '/html/body/div/div[1]/div/div/div/div/form/div[3]/textarea'
-    X_BUTTON_SEND_REVIEW = '/html/body/div/div[1]/div/div/div/div/form/button'
-    X_UPPER_REVIEW = '/html/body/div/div/div[4]/div[1]/div/div/div[2]/div[1]/div'
-    X_UPPER_REVIEW_TITLE = '/html/body/div/div/div[4]/div[1]/div/div/div[2]/div[1]/div/div[2]'
-    X_UPPER_REVIEW_TEXT = '/html/body/div/div/div[4]/div[1]/div/div/div[2]/div[1]/div/div[3]'
-    TEXT = '200IQ text'
-    TITLE = 'title'
+    CLASS_NAME_BUTTON_PLUS = 'about-film__button_plus'
+    CLASS_NAME_BUTTON_BOOKMARK = 'about-film__button_bookmark'
+    CLASS_NAME_BUTTON_TRAILER = 'js-btn-watch-trailer'
+    CLASS_NAME_PRODUCER = 'about-film__director'
+    CLASS_NAME_PRODUCER_ON_PAGE = 'actor-profile__name'
+    CLASS_NAME_TOSTER = 'js-errorMessage'
+    CLASS_NAME_TRAILER_CONTAINER = 'trailer__iframe'
+    CLASS_NAME_BUTTON_RATE_9 = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[2]/div[1]/button[2]'
+    CLASS_NAME_BUTTON_RATE_1 = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[2]/div[1]/button[10]'
+    CLASS_NAME_BUTTON_RATE_CONTAINER = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[2]/div[1]'
+    CLASS_NAME_BUTTON_RATE_DELETE = '/html/body/div/div/div[2]/div[2]/div[2]/form/div/div[4]/button'
+    CLASS_NAME_BUTTON_REVIEW = 'js-list-reviews__btn-write-review'
 
     def test_click_producer(self):
         self.render(f'{DOMAIN}/film/1')
 
-        prod = self.find((By.XPATH, self.X_PRODUCER), 3).text.strip()
-        self.find((By.XPATH, self.X_PRODUCER), 5).click()
+        prod = self.find((By.CLASS_NAME, self.CLASS_NAME_PRODUCER), 3).text.strip()
+        self.find((By.CLASS_NAME, self.CLASS_NAME_PRODUCER), 5).click()
 
-        prod_on_page = self.find((By.XPATH, self.X_PRODUCER_ON_PAGE), 3).text.strip()
+        prod_on_page = self.find((By.CLASS_NAME, self.CLASS_NAME_PRODUCER_ON_PAGE), 3).text.strip()
         if prod != prod_on_page:
             raise Exception("names does not equal", prod, prod_on_page)
 
     def test_click_plus_unauth(self):
         self.render(f'{DOMAIN}/film/1')
 
-        self.find((By.XPATH, self.X_BUTTON_PLUS)).click()
-        self.find((By.XPATH, self.X_TOSTER))
-        self.wait_hide((By.XPATH, self.X_TOSTER))
+        self.find((By.CLASS_NAME, self.CLASS_NAME_BUTTON_PLUS)).click()
+        self.find((By.CLASS_NAME, self.CLASS_NAME_TOSTER))
+        self.wait_hide((By.CLASS_NAME, self.CLASS_NAME_TOSTER))
 
-        self.find((By.XPATH, self.X_BUTTON_BOOKMARK)).click()
-        self.find((By.XPATH, self.X_TOSTER))
-        self.wait_hide((By.XPATH, self.X_TOSTER))
+        self.find((By.CLASS_NAME, self.CLASS_NAME_BUTTON_BOOKMARK)).click()
+        self.find((By.CLASS_NAME, self.CLASS_NAME_TOSTER))
+        self.wait_hide((By.CLASS_NAME, self.CLASS_NAME_TOSTER))
 
     def test_click_trailer(self):
         self.render(f'{DOMAIN}/film/1')
-        self.find((By.XPATH, self.X_BUTTON_TRAILER)).click()
-        self.find((By.XPATH, self.X_TRAILER_CONTAINER))
+        self.find((By.CLASS_NAME, self.CLASS_NAME_BUTTON_TRAILER)).click()
+        self.find((By.CLASS_NAME, self.CLASS_NAME_TRAILER_CONTAINER))
 
     def test_click_review_unauth(self):
         self.render(f'{DOMAIN}/film/1')
 
-        self.find((By.XPATH, self.X_BUTTON_REVIEW)).click()
+        self.find((By.CLASS_NAME, self.CLASS_NAME_BUTTON_REVIEW)).click()
 
-        self.find((By.XPATH, self.X_TOSTER))
-        self.wait_hide((By.XPATH, self.X_TOSTER))
+        self.find((By.CLASS_NAME, self.CLASS_NAME_TOSTER))
+        self.wait_hide((By.CLASS_NAME, self.CLASS_NAME_TOSTER))
--- hw/code/test_search.py
+++ hw/code/test_search.py
@@ -9,56 +9,56 @@ from const import *
 from helper_auth import needed_auth
 
 class TestSearch(BasePage):
-    X_INPUT_SEARCH = 'js-header__form__input'
-    X_BUTTON_RETURN_MAIN = 'search-page__no-content-btn'
-    X_TITLE_SEARCH = 'search-page__title'
+    CLASS_NAME_INPUT_SEARCH = 'js-header__form__input'
+    CLASS_NAME_BUTTON_RETURN_MAIN = 'search-page__no-content-btn'
+    CLASS_NAME_TITLE_SEARCH = 'search-page__title'
     TITLE_SEARCH = 'Результаты поиска'
     ALL_GROUP_SEARCH = 'a'
-    X_SEARCH_GROUP_FILMS = 0
+    CLASS_NAME_SEARCH_GROUP_FILMS = 0
     SEARCH_GROUP_FILMS = 'Найденные фильмы:'
-    X_CATEGORY_TITLE = 'search-list__title'
-    X_SEARCH_GROUP_SERAILS = 1
+    CLASS_NAME_CATEGORY_TITLE = 'search-list__title'
+    CLASS_NAME_SEARCH_GROUP_SERAILS = 1
     SEARCH_GROUP_SERAILS = 'Найденные сериалы:'
-    X_SEARCH_GROUP_PERSONS = 2
+    CLASS_NAME_SEARCH_GROUP_PERSONS = 2
     SEARCH_GROUP_PERSONS = 'Найденные имена:'
-    X_FIRST_FOUNDED_FILM = 'premiere-film__blackout'
-    X_FIRST_FOUNDED_TITLE = 'premiere-film__information-title'
-    X_TITLE_ON_FILM_PAGE = 'about-film__title'
+    CLASS_NAME_FIRST_FOUNDED_FILM = 'premiere-film__blackout'
+    CLASS_NAME_FIRST_FOUNDED_TITLE = 'premiere-film__information-title'
+    CLASS_NAME_TITLE_ON_FILM_PAGE = 'about-film__title'
 
     def test_check_exists(self):
         self.render(DOMAIN)
-        self.find((By.CLASS_NAME, self.X_INPUT_SEARCH)).send_keys(Keys.ENTER)
-        title = self.find((By.CLASS_NAME, self.X_TITLE_SEARCH)).text
+        self.find((By.CLASS_NAME, self.CLASS_NAME_INPUT_SEARCH)).send_keys(Keys.ENTER)
+        title = self.find((By.CLASS_NAME, self.CLASS_NAME_TITLE_SEARCH)).text
         if title != self.TITLE_SEARCH:
             raise Exception("stings does not equal", title, self.TITLE_SEARCH)
 
     def test_check_empty(self):
         self.render(DOMAIN)
-        self.find((By.CLASS_NAME, self.X_INPUT_SEARCH)).send_keys(Keys.ENTER)
-        self.find((By.CLASS_NAME, self.X_BUTTON_RETURN_MAIN)).click()
+        self.find((By.CLASS_NAME, self.CLASS_NAME_INPUT_SEARCH)).send_keys(Keys.ENTER)
+        self.find((By.CLASS_NAME, self.CLASS_NAME_BUTTON_RETURN_MAIN)).click()
 
     def test_check_group_category(self):
         self.render(DOMAIN)
-        self.find((By.CLASS_NAME, self.X_INPUT_SEARCH)).send_keys(self.ALL_GROUP_SEARCH, Keys.ENTER)
+        self.find((By.CLASS_NAME, self.CLASS_NAME_INPUT_SEARCH)).send_keys(self.ALL_GROUP_SEARCH, Keys.ENTER)
 
-        films_field = self.find_group((By.CLASS_NAME, self.X_CATEGORY_TITLE))[self.X_SEARCH_GROUP_FILMS].text
+        films_field = self.find_group((By.CLASS_NAME, self.CLASS_NAME_CATEGORY_TITLE))[self.CLASS_NAME_SEARCH_GROUP_FILMS].text
         if films_field != self.SEARCH_GROUP_FILMS:
             raise Exception("stings does not equal", films_field, self.SEARCH_GROUP_FILMS)
 
-        serials_field = self.find_group((By.CLASS_NAME, self.X_CATEGORY_TITLE))[self.X_SEARCH_GROUP_SERAILS].text
+        serials_field = self.find_group((By.CLASS_NAME, self.CLASS_NAME_CATEGORY_TITLE))[self.CLASS_NAME_SEARCH_GROUP_SERAILS].text
         if serials_field != self.SEARCH_GROUP_SERAILS:
             raise Exception("stings does not equal", serials_field, self.SEARCH_GROUP_SERAILS)
 
-        persons_field = self.find_group((By.CLASS_NAME, self.X_CATEGORY_TITLE))[self.X_SEARCH_GROUP_PERSONS].text
+        persons_field = self.find_group((By.CLASS_NAME, self.CLASS_NAME_CATEGORY_TITLE))[self.CLASS_NAME_SEARCH_GROUP_PERSONS].text
         if persons_field != self.SEARCH_GROUP_PERSONS:
             raise Exception("stings does not equal", persons_field, self.SEARCH_GROUP_PERSONS)
 
     def test_correct_results(self):
         self.render(DOMAIN)
-        self.find((By.CLASS_NAME, self.X_INPUT_SEARCH)).send_keys(self.ALL_GROUP_SEARCH, Keys.ENTER)
+        self.find((By.CLASS_NAME, self.CLASS_NAME_INPUT_SEARCH)).send_keys(self.ALL_GROUP_SEARCH, Keys.ENTER)
 
-        films_field = self.find((By.CLASS_NAME, self.X_FIRST_FOUNDED_TITLE)).text
-        self.find((By.CLASS_NAME, self.X_FIRST_FOUNDED_FILM)).click()
-        film_title = self.find((By.CLASS_NAME, self.X_TITLE_ON_FILM_PAGE)).text
+        films_field = self.find((By.CLASS_NAME, self.CLASS_NAME_FIRST_FOUNDED_TITLE)).text
+        self.find((By.CLASS_NAME, self.CLASS_NAME_FIRST_FOUNDED_FILM)).click()
+        film_title = self.find((By.CLASS_NAME, self.CLASS_NAME_TITLE_ON_FILM_PAGE)).text
         if films_field != film_title:
             raise Exception("stings does not equal", films_field, film_title)

c33fee30826950a3e2cccb68e28382c719c3efde
Ivan Biryukov <vanyabiryukov@yandex.ru>
2023-05-15 00:37:27 +0300

    all green, update artifacts

--- runner_tests.sh
+++ runner_tests.sh
@@ -2,8 +2,6 @@ export LOGIN=$1
 export PASSWORD=$2
 echo $LOGIN $PASSWORD
 
-pytest hw/code/test_login.py
-pytest hw/code/test_signup.py
 pytest hw/code/test_profile.py
 
 pytest hw/code/test_collection.py
@@ -16,3 +14,6 @@ pytest hw/code/test_film_auth.py
 pytest hw/code/test_navigation.py
 pytest hw/code/test_mainpage.py
 pytest hw/code/test_premierepage.py
+
+pytest hw/code/test_login.py
+pytest hw/code/test_signup.py
